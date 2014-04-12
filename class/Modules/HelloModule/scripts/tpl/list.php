ad2b1e3f50d22f0fee1949c924c5be0d341bf449
Jirka Daněk <jirka.danek@jdanek.eu>
2014-04-13 00:39:24 +0200

    přidána css třída pro bread list, sloupec Akce

--- class/Modules/HelloModule/scripts/tpl/list.php
+++ class/Modules/HelloModule/scripts/tpl/list.php
@@ -1,1 +1,39 @@
-<div class="bread-list-before">    <div class="new-item"><?php $this->renderLink('new', 'vytvořit nový záznam', 'create', null, 'list') ?></div>    <?php echo $paging[0]; ?>    <div class="filter"><select><option>Filtrování položek</option></select></div>    <div style='clear: both'></div></div><table class="list" width="100%">    <thead>        <tr>            <td class="minimal-width">#</td>            <td class="minimal-width">Username</td>            <td class="minimal-width">Email</td>            <td><!-- placeholder //--></td>            <td>Akce</td>        </tr>    </thead>    <tbody>        <?php while ($item = DB::row($result)): ?>            <tr>                <td><?php $this->renderStr($item['id']) ?></td>                <td><?php $this->renderStr($item['username']) ?></td>                <td><?php $this->renderStr($item['email']) ?></td>                <td><!-- placeholder //--></td>                <!-- AKCE //-->                <td class="bread-actions">                    <?php $this->renderLink('edit', '<span>editovat</span>', 'edit', array($item['id']), $self, array('class' => 'list-action-btn')) ?>                    <?php $this->renderLink('delete3', '<span>smazat</span>', 'del', array($item['id']), $self, array('class' => 'list-action-btn')) ?>                </td>            </tr>        <?php endwhile ?>    </tbody></table><?php echo $paging[0]; ?>
+<div class="bread-list-before">
+    <div class="new-item"><?php $this->renderLink('new', 'vytvořit nový záznam', 'create', null, 'list') ?></div>
+    <?php echo $paging[0]; ?>
+    <div class="filter"><select><option>Filtrování položek</option></select></div>
+    <div style='clear: both'></div>
+</div>
+
+<table class="list" width="100%">
+
+    <thead>
+        <tr>
+            <td class="minimal-width">#</td>
+            <td class="minimal-width">Username</td>
+            <td class="minimal-width">Email</td>
+            <td><!-- placeholder //--></td>
+            <td class="minimal-width">Akce</td>
+        </tr>
+    </thead>
+
+    <tbody>
+        <?php while ($item = DB::row($result)): ?>
+            <tr>
+                <td><?php $this->renderStr($item['id']) ?></td>
+                <td><?php $this->renderStr($item['username']) ?></td>
+                <td><?php $this->renderStr($item['email']) ?></td>
+                <td><!-- placeholder //--></td>
+
+                <!-- AKCE //-->
+                <td class="bread-actions">
+                    <?php $this->renderLink('edit', '<span>editovat</span>', 'edit', array($item['id']), $self, array('class' => 'list-action-btn')) ?>
+                    <?php $this->renderLink('delete3', '<span>smazat</span>', 'del', array($item['id']), $self, array('class' => 'list-action-btn')) ?>
+                </td>
+            </tr>
+        <?php endwhile ?>
+    </tbody>
+
+</table>
+
+<?php echo $paging[0]; ?>
